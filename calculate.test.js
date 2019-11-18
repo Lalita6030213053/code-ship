6b05a6f8e7efaac4de1cf053bbfaf836fdec2e23
Lalita6030213053 <ja.ea5923@gmail.com>
2019-11-18 21:21:08 +0700

    ++++++

---
+++ calculate.test.js
@@ -0,0 +1,13 @@
+  
+const calculator = require('./calculate');
+
+describe("Add function", () => {
+
+    test("1 + 1 = 2", () => {
+        const a = 1;
+        const b = 1;
+
+        const result = calculator.add(a, b);
+        expect(result).toEqual(2);
+    });
+});

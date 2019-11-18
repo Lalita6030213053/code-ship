4d4e9f2f661fc3d661fac2ed6065443815b0cc53
Lalita6030213053 <ja.ea5923@gmail.com>
2019-11-18 21:10:00 +0700

    +

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://86dc755e21594be2b8bb81c16247b07e@sentry.io/1824767' });
 
-// Sentry.configureScope(function(scope) {
-//     scope.setTag("my-tag", "6030213053");
-//     scope.setUser({
-//       email: "john.doe@example.com",
-//       username: 'Lalita Sathansat'
-//     });
-//    });
+Sentry.configureScope(function(scope) {
+    scope.setTag("my-tag", "6030213053");
+    scope.setUser({
+      email: "john.doe@example.com",
+      username: 'Lalita Sathansat'
+    });
+   });
    
 const calculator = require('./calculate');
 
